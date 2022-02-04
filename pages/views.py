0f1af34ca0c8f42c71e1edcac773668de9d5ce50
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-02-04 15:23:07 +0330

    change views and add page.html in project

--- pages/views.py
+++ pages/views.py
@@ -1,1 +1,1 @@
-/annex/objects/SHA256E-s153--f67e466e169833fe5db6107e9ed17756e84952fd67843f7b08e942c865d6ebb0.py
+/annex/objects/SHA256E-s203--16a45f731edf4b0b886db750e28a8f2a1ca9d073ad013496ee7b16ab9a4d6aff.py
